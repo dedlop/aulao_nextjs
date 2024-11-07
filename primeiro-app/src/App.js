f2da1c366e05912946a6a2b45bd00cc483a8a41f
dedlop <104914772+dedlop@users.noreply.github.com>
2024-11-07 11:26:52 -0300

    Revisão React JS

---
+++ primeiro-app/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+import Nome from './components/Nome';
+
+function App(){
+  const [aluno, setAluno] = useState('Sujeito Programador')
+
+  function handleChangeName(nome){
+    setAluno(nome);
+  }
+
+  return(
+    <div>
+      <h1>Componente App</h1><br/>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={ () => handleChangeName('Lucas Oliveira') }>
+        Mudar Nome
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
+
